3a0b392209b0923ee399c339824b0d66bd38a3d5
Ivan Cao-Berg <icaoberg@psc.edu>
2022-02-10 21:10:11 -0500

    Added scripts for version 3.1.20220210171524

---
+++ 3.1.20220210171524/modulefile.lua
@@ -0,0 +1,34 @@
+--
+-- cwltool 3.1.20220210171524 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: This is the reference implementation of the Common Workflow Language."
+-- "Keywords: singularity utilities"
+
+whatis("Name: cwltool")
+whatis("Version: 3.1.20220210171524")
+whatis("Category: Other")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: This is the reference implementation of the Common Workflow Language.")
+
+help([[
+This is the reference implementation of the Common Workflow Language.
+
+To load the module type
+
+> module load cwltool/3.1.20220210171524
+
+To unload the module type
+
+> module unload cwltool/3.1.20220210171524
+
+Tools included in this module are
+
+* cwltool
+]])
+
+local package = "cwltool"
+local version = "3.1.20220210171524"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
---
+++ 3.1.20220210171524/rbuild.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
+IMAGE=singularity-cwltool-3.1.20220210171524.sif
+DEFINITION=Singularity
+
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
+singularity build --remote $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
